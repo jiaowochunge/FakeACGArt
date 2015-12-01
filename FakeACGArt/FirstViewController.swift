0233635ec65eef61134715f65c79ded3883898d5
john <john@johndemacbook-pro-2.local>
2015-12-01 22:35:35 +0800

    Initial Commit

---
+++ FakeACGArt/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FakeACGArt
+//
+//  Created by john on 15/12/1.
+//  Copyright © 2015年 john. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
